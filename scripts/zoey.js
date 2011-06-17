6c70fdfa9ebb6d4a484ddcb5cb3704d06d8acb9e
Stan Angeloff <stanimir@psp-webtech.co.uk>
2011-06-18 01:22:49 +0300

    Add styling for ui-button, ui-header, ui-footer, ui-navigation, ui-body, ui-content, support for icons and themes.

--- scripts/zoey.js
+++ scripts/zoey.js
@@ -28,10 +28,10 @@ var $topPage;
 var $visiblePage;
 var $hiddenPage;
 var $cachedPages = {};
+var $highlight = [];
 var pageSequence = 0;
 var pageHash;
 var $fixedWidgets = [];
-var SCROLLTOP = 'zoey:scroll-top';
 function scrollTop(offset) {
   if (arguments.length) {
     document.body.scrollTop = document.documentElement.scrollTop = window.pageYOffset = offset;
@@ -40,22 +40,25 @@ function scrollTop(offset) {
   }
 };
 self.showPage = function($page, options) {
-  if ($visiblePage && $visiblePage.attr('id') === $page.attr('id')) {
+  var id = $page.attr('id');
+  if ($visiblePage && $visiblePage.attr('id') === id) {
     return self;
   }
   options || (options = {});
-  ($hiddenPage && $hiddenPage.attr('id') !== $page.attr('id')) && $hiddenPage.trigger('pagebeforehide').trigger('pagehide');
+  ($hiddenPage && $hiddenPage.attr('id') !== id) && $hiddenPage.trigger('pagebeforehide').trigger('pagehide');
   if (options.type === 'dialog') {
     $hiddenPage = $visiblePage;
-    $visiblePage && $visiblePage.data(SCROLLTOP, scrollTop()).addClass('collapsed');
+    $visiblePage && $visiblePage.data('zoey:scroll-top', scrollTop()).addClass('ui-collapsed');
   } else {
     $hiddenPage = null;
-    $visiblePage && $visiblePage.data(SCROLLTOP, scrollTop()).trigger('pagebeforehide').addClass('collapsed').trigger('pagehide');
+    $visiblePage && $visiblePage.data('zoey:scroll-top', scrollTop()).trigger('pagebeforehide').addClass('ui-collapsed').trigger('pagehide');
   }
-  $visiblePage = $page.trigger('pagebeforeshow').removeClass('collapsed').trigger('pageshow');
+  $highlight.length && $highlight.removeClass('ui-highlight');
+  $highlight = $page.find('[href="#' + id + '"]').addClass('ui-highlight');
+  $visiblePage = $page.trigger('pagebeforeshow').removeClass('ui-collapsed').trigger('pageshow');
   setTimeout(function() {
-    scrollTop($visiblePage.data(SCROLLTOP) || 0);
-  }, 25);
+    scrollTop($visiblePage.data('zoey:scroll-top') || 0);
+  }, 125);
   options.event && options.event.stopPropagation();
   $page.get(0).focus();
   return self;
@@ -89,28 +92,67 @@ self.widgets = {
     }).delegate('form', 'submit', function(event) {
     });
     $topPage || ($topPage = this);
-    this.addClass('collapsed');
+    this.addClass('ui-collapsed');
   },
   navigation: function() {
     var $children = this.children();
-    $children.css({ width: (100 / $children.length) + '%' });
+    $children.addClass('ui-size-' + String.fromCharCode(97 /* 'a' */ + $children.length - 1));
+  },
+  content: function() {
+  },
+  list: function() {
+  },
+  group: function() {
+  },
+  collapsible: function() {
+    var $this = this;
+    var $first = $this.children().first();
+    var icons = ['arrow-d', 'arrow-r'];
+    ($this.data('collapsed') === 'true') && $this.addClass('ui-closed');
+    self.role.call($first.data('icon', $this.hasClass('ui-closed') ? icons[1] : icons[0]), 'button');
+    $first.bind('click', function() {
+      $this.toggleClass('ui-closed');
+      if ($this.hasClass('ui-closed')) {
+        $first.removeClass('ui-icon-' + icons[0]).addClass('ui-icon-' + icons[1]);
+      } else {
+        $first.removeClass('ui-icon-' + icons[1]).addClass('ui-icon-' + icons[0]);
+      }
+    });
+  },
+  footer: function() {
   }
 };
 self.role = function(role) {
   var $control = this;
   var widget = self.widgets[role];
-  $control.hasClass('ui-' + role) || (widget && widget.call($control.addClass('ui-' + role)));
+  $control.hasClass('ui-' + role) || (widget && widget.call($control));
+  $control.addClass('ui-widget ui-' + role);
   ['theme', 'icon'].forEach(function(type) {
     $control.data(type) && $control.addClass('ui-has-' + type + ' ui-' + type + '-' + $control.data(type));
   });
+  var themeRegExp = /\bui-theme-(\w+)\b/;
+  var getTheme = function() { var list = themeRegExp.exec(this.className); return (list && list[1]); };
+  var parent = $control.get(0);
+  var inheritedTheme;
+  do {
+    inheritedTheme = $(parent).data('theme');
+    if ( ! inheritedTheme && parent.className.indexOf('ui-theme-') >= 0) {
+      inheritedTheme = getTheme.call(parent);
+    }
+    if (inheritedTheme) {
+      break;
+    }
+  } while ((parent = parent.parentNode) && (parent !== document));
+  inheritedTheme || (inheritedTheme = getTheme.call(document.documentElement));
+  inheritedTheme && $control.addClass('ui-inherit-theme-' + inheritedTheme);
 };
 self.initialize = function(scope, options) {
   options || (options = {});
+  scope || $('html').addClass('ui-theme-' + (options.theme || 'c'));
   (scope || $('html')).find('[data-role], input[type="button"], input[type="submit"], button').each(function() {
     self.role.apply($(this), [$(this).data('role') || 'button']);
   });
   if ( ! scope) {
-    $('html').addClass('ui-theme-' + (options.theme || 'c'));
     $(window).bind('hashchange', function(event) {
       var value = location.hash.replace(/^#/, '');
       if (pageHash !== value) {
